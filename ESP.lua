e73f9eadd20f7fd1b2ab6305458d55ec380201f7
F1K2 <dustinlepro2010@gmail.com>
2025-02-08 01:30:04 +0100

    Update and rename ESP to ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,6 +1,6 @@
 _G.FriendColor = Color3.fromRGB(0, 0, 255)
 _G.EnemyColor = Color3.fromRGB(255, 0, 0)
-_G.UseTeamColor = true
+_G.UseTeamColor = false
 
 --------------------------------------------------------------------
 local Holder = Instance.new("Folder", game.CoreGui)
